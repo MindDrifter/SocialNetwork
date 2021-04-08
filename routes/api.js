7a73f88249fbc8c95d7a19642b6c65a20d5bee92
TheWorstOfTheWorst <forbl1zzard@mail.ru>
2021-04-09 09:15:02 +1000

    New commit!

--- routes/api.js
+++ routes/api.js
@@ -13,13 +13,37 @@ router.get('/', async (req, res)=>{
 })
 
 //POST
-router.post('/', async(req, res)=>{
-    const model = new NewModel({
-        name: req.body.name,
-        token: sha.sha256(req.body.password+req.body.title)
+router.post('/register', async(req, res)=>{
+    var login = req.body.login.trim()
+    var password = req.body.password.trim()
+    var token = sha.sha256(login+password)
+    
+    if(await (await NewModel.find({"token":token})).length > 0){
+        res.status(200).send('Такой аккаунт уже существует')
+    }else{
+        const model = new NewModel({
+            login: login ,
+            token: token
+        })
+        await model.save()
+        res.status(201).send({
+            msg:'Вы успешно зарегестрировались',
+            token:token
+        })
+    }
+})
+
+router.post('/login', async(req, res)=>{
+    var login = req.body.login.trim()
+    var password = req.body.password.trim() 
+    var token = sha.sha256(login+password)
+    
+    var value = await NewModel.find({
+        "token":token
     })
-    await model.save()
-    res.status(201).send('Успешная регистрация')
+    res.status(200).json({
+        msg:'Вы успешно вошли',
+        token:value[0].token})
 })
 
 //DELETE
